599a158208856e1aee1e2a4e5527f5b4430e2a2a
m2rads <argo.mohrad@gmail.com>
2023-04-03 00:01:31 -0700

    feat: add doc embedding to create emebedding for eahc page of the book

--- lib/scripts/pdf_to_pages_embeddings.rb
+++ lib/scripts/pdf_to_pages_embeddings.rb
@@ -4,6 +4,13 @@ require 'tempfile'
 require 'optparse'
 require 'daru'
 require 'csv'
+require_relative '../../app/services/openai_service.rb'
+
+COMPLETIONS_MODEL = "text-davinci-003"
+
+MODEL_NAME = "curie"
+
+DOC_EMBEDDINGS_MODEL = "text-search-#{MODEL_NAME}-doc-001"
 
 $de_tokenizer = Tokenizer::WhitespaceTokenizer.new
 
@@ -54,3 +61,26 @@ CSV.open("#{filename}.pages.csv", "wb") do |csv|
     csv << [row[:title], row[:content], row[:tokens]]
   end
 end
+
+def get_doc_embedding(text) 
+    return OpenAIService.instance.get_embedding(text, DOC_EMBEDDINGS_MODEL)
+end 
+
+def compute_doc_embeddings(df)
+    """
+    Create an embedding for each row in the dataframe using the OpenAI Embeddings API.
+  
+    Return a dictionary that maps between each embedding vector and the index of the row that it corresponds to.
+    """
+    embeddings = {}
+    idx = 1
+    df.each_row_with_index do |r, idx|
+        embeddings[idx] = get_doc_embedding(r[:content])
+        # puts r[:content]
+      end
+    return embeddings
+end
+
+doc_embeddings = compute_doc_embeddings(df)
+
+puts doc_embeddings
