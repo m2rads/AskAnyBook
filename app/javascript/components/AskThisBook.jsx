9e0e181274574dfa04e273b86d685ba69a1ff4cb
m2rads <argo.mohrad@gmail.com>
2023-04-02 18:42:35 -0700

    add form container to main component

--- app/javascript/components/AskThisBook.jsx
+++ app/javascript/components/AskThisBook.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import BookCover from "./common/BookCover";
+import FormContainer from "./common/FormContainer";
 import "../styles/styles.css";
 
 const AskTheBook = (props) => (
@@ -8,6 +9,10 @@ const AskTheBook = (props) => (
       <div className="logo">
         <BookCover url={"/assets/getting-real-cover.png"} />
         <h1>Ask This Book</h1>
+      </div>
+      <div className="main">
+        <p className="credits">Getting Real</p>
+        <FormContainer question={props.book.question_text} />
       </div>
     </div>
   </div>
