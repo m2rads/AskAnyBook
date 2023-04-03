339f85cc53423ef6939784a0c6cf8864deb0e3da
m2rads <argo.mohrad@gmail.com>
2023-04-03 10:51:43 -0700

    feat: add vecotr similarity and load embeddings from csv file

--- app/services/embedding_service.rb
+++ app/services/embedding_service.rb
@@ -1,5 +1,8 @@
 require_relative './openai_service.rb'
 require 'Singleton'
+require 'numo/narray'
+require 'csv'
+require 'daru'
 
 class EmbeddingService
     include Singleton
@@ -19,4 +22,32 @@ class EmbeddingService
     def get_query_embedding(text)
         return OpenAIService.instance.get_embedding(text, QUERY_EMBEDDINGS_MODEL)
     end
+
+    def vector_similarity(x, y)
+        x_array = Numo::NArray.cast(x)
+        y_array = Numo::NArray.cast(y)
+        
+        return x_array.dot(y_array)
+    end
+
+    def load_embeddings(fname)
+        """
+        Read the document embeddings and their keys from a CSV.
+      
+        fname is the path to a CSV with exactly these named columns:
+            up to the length of the embedding vectors.
+        """
+
+        data = {}
+        max_dim = 0
+        CSV.foreach(fname, headers: true) do |row|
+          title = row["title"]
+          embedding = row.select { |k, v| k != "title" }.map { |k, v| v.to_f }
+          max_dim = embedding.size - 1 if embedding.size - 1 > max_dim
+          data[title] = embedding
+        end
+        data.transform_values! { |v| v + [0.0] * (max_dim - v.size + 1) }
+        return data
+    end
+
 end 
