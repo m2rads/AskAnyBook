64280707644f9b882d2bbe9e5fa1076536c1fe7e
m2rads <argo.mohrad@gmail.com>
2023-04-02 18:16:28 -0700

    add BookCover component to the app entry point

--- app/javascript/components/AskThisBook.jsx
+++ app/javascript/components/AskThisBook.jsx
@@ -1,4 +1,16 @@
 import React from "react";
+import BookCover from "./common/BookCover";
 import "../styles/styles.css";
-const HelloMessage = ({ name }) => <h1>Hello, {name}!</h1>;
-export default HelloMessage;
+
+const AskTheBook = (props) => (
+  <div>
+    <div className="header">
+      <div className="logo">
+        <BookCover url={"/assets/getting-real-cover.png"} />
+        <h1>Ask This Book</h1>
+      </div>
+    </div>
+  </div>
+);
+
+export default AskTheBook;
