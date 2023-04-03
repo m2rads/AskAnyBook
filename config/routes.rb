db35d73fe111e482c44a94474395c6dace7fa88f
m2rads <argo.mohrad@gmail.com>
2023-04-02 17:44:22 -0700

    change application entry to root endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'site/index'
+  # get 'site/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'site#index'
+
 end
