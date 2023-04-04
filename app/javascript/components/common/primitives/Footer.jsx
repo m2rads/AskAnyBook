85908509910204fa4c8d911a4d7f5e2ff4d5db8c
m2rads <argo.mohrad@gmail.com>
2023-04-03 17:58:27 -0700

    add footer section

---
+++ app/javascript/components/common/primitives/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p className="credits">
+        Project inspired by <a href="https://askmybook.com">Askmybook.com</a> •{" "}
+        <a href="https://github.com/slavingia/askmybook">Fork on GitHub</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
