c5458a887ffbf4ca723fe9a619164a85625369a9
m2rads <argo.mohrad@gmail.com>
2023-04-04 11:56:27 -0700

    fix missing ReactDom import

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import AskThisBook from "./components/AskThisBook";
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
   const node = document.getElementById("book_question");
+  if (!node) {
+    console.error('Element with ID "book_question" not found');
+    return;
+  }
   const data = JSON.parse(node.getAttribute("data"));
-  ReactDOM.render(
-    <AskThisBook book={data} />,
-    document.body.appendChild(document.createElement("div"))
-  );
+
+  ReactDOM.render(<AskThisBook book={data} />, document.getElementById("root"));
 });
