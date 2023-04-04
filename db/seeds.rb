afecd47265df8e5473054b43b5caca3fcd9e1c89
m2rads <argo.mohrad@gmail.com>
2023-04-03 18:06:18 -0700

    add seed data to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,21 @@
 #   Character.create(name: "Luke", movie: movies.first)
 question = Question.create(
     question_text: "What is the book Getting Real about?", 
-    answer: "test"
+    answer: "Getting Real is a smaller, faster, better way to build 
+    software. It focuses on big-picture ideas to help entrepreneurs, 
+    designers, programmers, and marketers build successful web apps. 
+    It begins with the customer experience and builds backwards, 
+    favoring iterations and sensible decision-making over traditional 
+    functional specs and paperwork. It's an approach suited to 
+    web-based applications, and can be applied to many different 
+    types of endeavors."
 )
 
 question_2 = Question.create(
     question_text: "How to deliver a software to customers faster", 
-    answer: "test"
+    answer: "Focus on shipping minimal viable products (MVPs) and 
+    try to reduce the complexity of your warehouse or cloud setup.
+    Start small before scaling, use version control for tracking 
+    orders, automate as many processes as possible, and build with 
+    customer feedback in mind to iterate quickly."
 )
