a337b2448fbbd0c4fbcd18b4a5d7b779e9304e5b
m2rads <argo.mohrad@gmail.com>
2023-04-03 18:18:13 -0700

    add route handler to output the body of the request

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,4 +2,16 @@ class SiteController < ApplicationController
   def index
     @question = Question.find(1)
   end
+
+  def ask 
+    embedding_service = EmbeddingService.instance
+
+    question_asked = params[:question].presence || ""
+    if !question_asked.end_with?("?")
+      question_asked += "?"
+    end
+
+    puts question_asked
+    head :ok
+  end
 end
