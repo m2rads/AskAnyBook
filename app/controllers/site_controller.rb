ea6d28ae97a895e791852610477239ddeb10f86a
m2rads <argo.mohrad@gmail.com>
2023-04-03 18:30:21 -0700

    add server side caching for frequently asked questions

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -11,6 +11,21 @@ class SiteController < ApplicationController
       question_asked += "?"
     end
 
+    previous_question = Question.find_by(question_text: question_asked)
+
+    if previous_question
+      print "previously asked and answered \n === \n " + previous_question.answer + " \n==="
+      previous_question.ask_count = previous_question.ask_count + 1
+      print "\n ask count" + previous_question.ask_count.to_s + " \n=== "
+      previous_question.save()
+      render json: {
+        question: previous_question.question_text,
+        answer: previous_question.answer,
+        id: previous_question.id
+      }
+      return
+    end
+
     puts question_asked
     head :ok
   end
